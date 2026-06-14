03de916e7e8dc2c719b770ebb3d70ff13049e072
elena <kev7570@gmail.com>
2026-06-14 19:10:11 +0300

    Add home page with statistics

--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Mailing, Client
 
-# Create your views here.
+def home(request):
+    context = {
+        'total_mailings': Mailing.objects.count(),
+        'active_mailings': Mailing.objects.filter(status='Запущена').count(),
+        'unique_clients': Client.objects.count(),
+    }
+    return render(request, 'mailing/home.html', context)
